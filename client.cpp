6c41821f67704acc5874966b2ce2bf938e44e7e5
Illia Sierov <illia.sierov@globallogic.com>
2024-05-10 00:10:30 +0300

    Added some changes after code review

--- client.cpp
+++ client.cpp
@@ -14,9 +14,9 @@
 #define CLIENT_CONFIG "client.conf"
 
 struct ClientConfig {
-    std::string serverIP;
-    int serverPort;
-    double number;
+	std::string serverIP;
+	int serverPort;
+	double number;
 };
 
 void handleUniqueDoubles(int, struct sockaddr_in, socklen_t);
@@ -24,157 +24,154 @@ void writeVectorToBinaryFile(const std::vector<double>& vec, const std::string& 
 void readClientConfig(const std::string&, ClientConfig&);
 
 int main() {
-    int sockfd;
-    char server_answer[BUFFER_SIZE];
-    struct sockaddr_in server_addr;
-    struct ClientConfig conf;
+	int sockfd;
+	char serverAnswer[BUFFER_SIZE];
+	struct sockaddr_in serverAddr;
+	struct ClientConfig conf;
 
-    readClientConfig(CLIENT_CONFIG, conf);
+	readClientConfig(CLIENT_CONFIG, conf);
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        std::cerr << "Socket creation failed" << std::endl;
-        return -1;
-    }
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		std::cerr << "Socket creation failed" << std::endl;
+		return -1;
+	}
 
-    std::cout<<conf.number<<";"<<conf.serverIP<<";"<<conf.serverPort<<std::endl;
+	memset(&serverAddr, 0, sizeof(serverAddr));
 
-    memset(&server_addr, 0, sizeof(server_addr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(conf.serverPort);
+	serverAddr.sin_addr.s_addr = inet_addr(conf.serverIP.c_str());
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(conf.serverPort);
-    server_addr.sin_addr.s_addr = inet_addr(conf.serverIP.c_str());
-
-    int len, n;
-    std::string syn_msg = "SYN:";
-    std::string ack_msg = "ACK";
+	int len, n;
+	std::string syn_msg = "SYN:";
+	std::string ackMsg = "ACK";
  
-    syn_msg += CLIENT_PROTOCOL_VERSION;
-    sendto(sockfd, syn_msg.c_str(), syn_msg.length(), MSG_CONFIRM, (const struct sockaddr *)&server_addr, sizeof(server_addr));
-    n = recvfrom(sockfd, (char *)server_answer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, (socklen_t *)&len);
-    if (n <= 0) {
-        int max_attempts = 5;
-        int attempts = 0;
+	syn_msg += CLIENT_PROTOCOL_VERSION;
+	sendto(sockfd, syn_msg.c_str(), syn_msg.length(), MSG_CONFIRM, (const struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	n = recvfrom(sockfd, (char *)serverAnswer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&serverAddr, (socklen_t *)&len);
+	if (n <= 0) {
+		int maxAttempts = 5;
+		int attempts = 0;
 
-        while (attempts < max_attempts) {
-            n = recvfrom(sockfd, (char *)server_answer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, (socklen_t *)&len);
-            if (n <= 0) {
-                break;
-            }
+		while (attempts < maxAttempts) {
+			n = recvfrom(sockfd, (char *)serverAnswer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&serverAddr, (socklen_t *)&len);
+			if (n <= 0) {
+				break;
+			}
 
-            attempts++;
-            sleep(1);
-        }
+			attempts++;
+			sleep(1);
+		}
 
-        if (attempts == max_attempts) {
-            std::cerr << "Max connection attempts reached. Unable to connect to the server." << std::endl;
-            return -1;
-        }
-    }
-    
-    std::string server_msg = server_answer;
-    if (server_msg != ack_msg) {
-        std::cout << server_msg << std::endl;
-        return -1;
-    }
+		if (attempts == maxAttempts) {
+			std::cerr << "Max connection attempts reached. Unable to connect to the server." << std::endl;
+			return -1;
+		}
+	}
+	
+	std::string serverMsg = serverAnswer;
+	if (serverMsg != ackMsg) {
+		std::cout << serverMsg << std::endl;
+		return -1;
+	}
 
-    n = recvfrom(sockfd, (char *)server_answer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, (socklen_t *)&len);
-    if (n <= 0) {
-        int max_attempts = 5;
-        int attempts = 0;
+	n = recvfrom(sockfd, (char *)serverAnswer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&serverAddr, (socklen_t *)&len);
+	if (n <= 0) {
+		int maxAttempts = 5;
+		int attempts = 0;
 
-        while (attempts < max_attempts) {
-            n = recvfrom(sockfd, (char *)server_answer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, (socklen_t *)&len);
-            if (n <= 0) {
-                break;
-            }
+		while (attempts < maxAttempts) {
+			n = recvfrom(sockfd, (char *)serverAnswer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&serverAddr, (socklen_t *)&len);
+			if (n <= 0) {
+				break;
+			}
 
-            attempts++;
-            sleep(1);
-        }
+			attempts++;
+			sleep(1);
+		}
 
-        if (attempts == max_attempts) {
-            std::cerr << "Max connection attempts reached. Unable to connect to the server." << std::endl;
-            return -1;
-        }
-    }
-    char* endp;
-    int newPort = strtol(server_answer, &endp, 10);
-    server_addr.sin_port = htons(newPort);
-    sleep(3);
+		if (attempts == maxAttempts) {
+			std::cerr << "Max connection attempts reached. Unable to connect to the server." << std::endl;
+			return -1;
+		}
+	}
+	char* endp;
+	int newPort = strtol(serverAnswer, &endp, 10);
+	serverAddr.sin_port = htons(newPort);
+	sleep(3);
 
-    std::string numberStr = std::to_string(conf.number);
-    sendto(sockfd, numberStr.c_str(), numberStr.length(), MSG_CONFIRM, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+	std::string numberStr = std::to_string(conf.number);
+	sendto(sockfd, numberStr.c_str(), numberStr.length(), MSG_CONFIRM, (const struct sockaddr *)&serverAddr, sizeof(serverAddr));
 
-    handleUniqueDoubles(sockfd, server_addr, len);
+	handleUniqueDoubles(sockfd, serverAddr, len);
 
-    close(sockfd);
-    return 0;
+	close(sockfd);
+	return 0;
 }
 
-void handleUniqueDoubles(int sockfd, struct sockaddr_in server_addr, socklen_t len)
+void handleUniqueDoubles(int sockfd, struct sockaddr_in serverAddr, socklen_t len)
 {
-    std::vector<double>  uniqueDoubles;
-    char buffer[BUFFER_SIZE];
-    int n;
+	std::vector<double>  uniqueDoubles;
+	char buffer[BUFFER_SIZE];
+	int n;
 
-    while(1)
-    {
-        n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, (socklen_t *)&len);
-        if (n <= 0) break;
-        buffer[n] = '\0';
-        std::cout<<buffer<<std::endl;
-        if(*buffer == 'E') break;
-        std::istringstream iss(buffer);
-        std::string token;
-        while (std::getline(iss, token, ';')) {
-            double num = std::stod(token);
-            uniqueDoubles.push_back(num);
-        }
-    }
-    std::sort(uniqueDoubles.begin(), uniqueDoubles.end(), std::greater<double>());
-    for (double num : uniqueDoubles) {
-        std::cout<<num<< ";";
-    }
-    writeVectorToBinaryFile(uniqueDoubles, OUTPUT_FILE_NAME);
+	while(1)
+	{
+		n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&serverAddr, (socklen_t *)&len);
+		if (n <= 0) break;
+		buffer[n] = '\0';
+		if(*buffer == 'E') break;
+		std::istringstream iss(buffer);
+		std::string token;
+		while (std::getline(iss, token, ';')) {
+			double num = std::stod(token);
+			uniqueDoubles.push_back(num);
+		}
+	}
+	std::sort(uniqueDoubles.begin(), uniqueDoubles.end(), std::greater<double>());
+	for (double num : uniqueDoubles) {
+		std::cout<<num<< ";";
+	}
+	writeVectorToBinaryFile(uniqueDoubles, OUTPUT_FILE_NAME);
 }
 
 void writeVectorToBinaryFile(const std::vector<double>& vec, const std::string& filename) {
-    std::ofstream file(filename, std::ios::binary);
-    if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file for writing." << std::endl;
-        return;
-    }
+	std::ofstream file(filename, std::ios::binary);
+	if (!file.is_open()) {
+		std::cerr << "Error: Unable to open file for writing." << std::endl;
+		return;
+	}
 
-    file.write(reinterpret_cast<const char*>(&vec[0]), vec.size() * sizeof(double));
+	file.write(reinterpret_cast<const char*>(&vec[0]), vec.size() * sizeof(double));
 
-    file.close();
+	file.close();
 }
 
 void readClientConfig(const std::string& filename, ClientConfig& config) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return;
-    }
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return;
+	}
 
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.find("Server ip") != std::string::npos) {
-            size_t pos = line.find(":");
-            if (pos != std::string::npos) {
-                config.serverIP = line.substr(pos + 2);
-            }
-        } else if (line.find("Server port") != std::string::npos) {
-            size_t pos = line.find(":");
-            if (pos != std::string::npos) {
-                config.serverPort = std::stoi(line.substr(pos + 2));
-            }
-        } else if (line.find("Number") != std::string::npos) {
-            size_t pos = line.find(":");
-            if (pos != std::string::npos) {
-                config.number = std::stod(line.substr(pos + 2));
-            }
-        }
-    }
+	std::string line;
+	while (std::getline(file, line)) {
+		if (line.find("Server ip") != std::string::npos) {
+			size_t pos = line.find(":");
+			if (pos != std::string::npos) {
+				config.serverIP = line.substr(pos + 2);
+			}
+		} else if (line.find("Server port") != std::string::npos) {
+			size_t pos = line.find(":");
+			if (pos != std::string::npos) {
+				config.serverPort = std::stoi(line.substr(pos + 2));
+			}
+		} else if (line.find("Number") != std::string::npos) {
+			size_t pos = line.find(":");
+			if (pos != std::string::npos) {
+				config.number = std::stod(line.substr(pos + 2));
+			}
+		}
+	}
 }
--- server.cpp
+++ server.cpp
@@ -19,204 +19,218 @@
 #define PORT_RANGE_START 1024
 #define PORT_RANGE_END 65535
 
-std::unordered_set<double> generateUniqueDoubles(double);
-void sendUniqueDoubles(int, struct sockaddr_in, socklen_t, std::unordered_set<double>);
+void generateUniqueDoubles(double, std::unordered_set<double>&);
+void sendUniqueDoubles(int, struct sockaddr_in, socklen_t, const std::unordered_set<double>&);
 void handleClient(int, struct sockaddr_in, socklen_t, const char*);
 int findAvailablePort(const std::unordered_set<int>&);
 int getPortFromFile(const std::string&);
 
-std::mutex mtx;
+std::mutex mtxThreads, mtxPorts;
 std::unordered_set<int> usedPorts;
 
 int main() {
-    int sockfd;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    std::vector<std::future<void>> futures;
-    int port = getPortFromFile(SERVER_CONFIG);
+	int sockfd;
+	char buffer[BUFFER_SIZE];
+	struct sockaddr_in serverAddr, clientAddr;
+	std::vector<std::future<void>> futures;
+	int port = getPortFromFile(SERVER_CONFIG);
 
-    std::cout<<port<<std::endl;
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		std::cerr << "Socket creation failed" << std::endl;
+		return -1;
+	}
 
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	memset(&clientAddr, 0, sizeof(clientAddr));
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        std::cerr << "Socket creation failed" << std::endl;
-        return -1;
-    }
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(port);
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    memset(&client_addr, 0, sizeof(client_addr));
+	if (bind(sockfd, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+		std::cerr << "Binding failed" << std::endl;
+		return -1;
+	}
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(port);
+	socklen_t len = sizeof(clientAddr);
+	int n;
 
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Binding failed" << std::endl;
-        return -1;
-    }
+	while (1) {
+		n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&clientAddr, &len);
+		if(n <= 0) continue;
+		buffer[n] = '\0';
 
-    socklen_t len = sizeof(client_addr);
-    int n;
+		futures.push_back(std::async(std::launch::async, [sockfd, clientAddr, len, buffer]() {
+			handleClient(sockfd, clientAddr, len, buffer);
+		}));
+	}
 
-    while (1) {
-        n = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &len);
-        if(n <= 0) continue;
-        buffer[n] = '\0';
-
-        futures.push_back(std::async(std::launch::async, [sockfd, client_addr, len, buffer]() {
-            handleClient(sockfd, client_addr, len, buffer);
-        }));
-    }
-
-    for (auto& future : futures) {
-        future.wait();
-    }
-    close(sockfd);
-    return 0;
+	for (auto& future : futures) {
+		future.wait();
+	}
+	close(sockfd);
+	return 0;
 }
 
-std::unordered_set<double> generateUniqueDoubles(double X) {
-    std::unordered_set<double> uniqueNumbers;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dis(-1.0 * X, X);
-    double num;
+void generateUniqueDoubles(double X, std::unordered_set<double>& uniqueNumbers) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<double> dis(-1.0 * X, X);
+	double num;
 
-    while (uniqueNumbers.size() < ARRAY_SIZE) {
-        num = dis(gen);
-        uniqueNumbers.insert(num);
-    }
-
-    return uniqueNumbers;
+	while (uniqueNumbers.size() < ARRAY_SIZE) {
+		num = dis(gen);
+		uniqueNumbers.insert(num);
+	}
 }
 
-void sendUniqueDoubles(int sockfd, struct sockaddr_in client_addr, socklen_t len, std::unordered_set<double> uniqueDoubles)
+void sendUniqueDoubles(int sockfd, struct sockaddr_in clientAddr, socklen_t len, const std::unordered_set<double>& uniqueDoubles)
 {
-    std::string dataStr;
+	std::string dataStr;
 
-    for (double num : uniqueDoubles) {
-        dataStr +=  std::to_string(num) + ";";
-        if (dataStr.size() + MAX_DOUBLE_LENGTH >= BUFFER_SIZE) {
-            sendto(sockfd, dataStr.c_str(), dataStr.length(), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
-            std::cout<<"Sended\n"<<dataStr;
-            dataStr.clear();
-        }
-    }
-    sendto(sockfd, "E", sizeof("E"), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
+	for (double num : uniqueDoubles) {
+		dataStr +=  std::to_string(num) + ";";
+		if (dataStr.size() + MAX_DOUBLE_LENGTH >= BUFFER_SIZE) {
+			sendto(sockfd, dataStr.c_str(), dataStr.length(), MSG_CONFIRM, (const struct sockaddr *)&clientAddr, len);
+			dataStr.clear();
+		}
+	}
+	sendto(sockfd, "E", sizeof("E"), MSG_CONFIRM, (const struct sockaddr *)&clientAddr, len);
 }
 
-void handleClient(int sockfd, struct sockaddr_in client_addr, socklen_t len, const char* buffer)
+void handleClient(int sockfd, struct sockaddr_in clientAddr, socklen_t len, const char* buffer)
 {
-    std::cout << "Function is executing in thread: " << std::this_thread::get_id() << std::endl;
+	std::cout << "Function is executing in thread: " << std::this_thread::get_id() << std::endl;
 
-    int i = 0;
-    std::string version;
+	int i = 0;
+	std::string version;
 
-    std::cout << "Client : " << buffer << std::endl;
-                
-    while (buffer[i] != ':' && buffer[i] != '\0') {
-        i++;
-    }
-    if (buffer[i] == '\0') {
-        std::string errorMsg = "Unsupported syn message format";
-        std::cout<<"Unsupported format: "<<buffer<<std::endl;
-        sendto(sockfd, errorMsg.c_str(), errorMsg.length(), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
-        return;
-    }
-    if (buffer[i] == ':') {
-        i++;
-        std::string clientVersion = buffer + i;
-        std::string serverVersion = SERVER_PROTOCOL_VERSION;
+	std::cout << "Client : " << buffer << std::endl;
+				
+	while (buffer[i] != ':' && buffer[i] != '\0') {
+		i++;
+	}
+	if (buffer[i] == '\0') {
+		std::string errorMsg = "Unsupported syn message format";
+		std::cout<<"Unsupported format: "<<buffer<<std::endl;
+		sendto(sockfd, errorMsg.c_str(), errorMsg.length(), MSG_CONFIRM, (const struct sockaddr *)&clientAddr, len);
+		return;
+	}
+	if (buffer[i] == ':') {
+		i++;
+		std::string clientVersion = buffer + i;
+		std::string serverVersion = SERVER_PROTOCOL_VERSION;
 
-        if (clientVersion != serverVersion) {
-            std::string errorMsg = "Wrong protocol version. SERVER_VERSION ";
+		if (clientVersion != serverVersion) {
+			std::string errorMsg = "Wrong protocol version. SERVER_VERSION ";
 
-            errorMsg += SERVER_PROTOCOL_VERSION;
-            sendto(sockfd, errorMsg.c_str(), errorMsg.length(), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
-            return;
-        }
-    }
-    int n;
-    char clientMsg[BUFFER_SIZE];
+			errorMsg += SERVER_PROTOCOL_VERSION;
+			sendto(sockfd, errorMsg.c_str(), errorMsg.length(), MSG_CONFIRM, (const struct sockaddr *)&clientAddr, len);
+			return;
+		}
+	}
 
-    sendto(sockfd, ACK_MSG, sizeof(ACK_MSG), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
-    mtx.lock();
-    int newPort = findAvailablePort(usedPorts);
-    std::string portString = std::to_string(newPort);
-    sendto(sockfd, portString.c_str(), portString.length(), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
+	char clientMsg[BUFFER_SIZE];
 
-    int newSocket;
-    struct sockaddr_in newServerAddr;
+	sendto(sockfd, ACK_MSG, sizeof(ACK_MSG), MSG_CONFIRM, (const struct sockaddr *)&clientAddr, len);
+	mtxThreads.lock();
+	int newPort = findAvailablePort(usedPorts);
+	std::string portString = std::to_string(newPort);
+	int newSocket;
+	struct sockaddr_in newServerAddr;
 
-    memset(&newServerAddr, 0, sizeof(newServerAddr));
+	sendto(sockfd, portString.c_str(), portString.length(), MSG_CONFIRM, (const struct sockaddr *)&clientAddr, len);
+	memset(&newServerAddr, 0, sizeof(newServerAddr));
 
-    newServerAddr.sin_family = AF_INET;
-    newServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    newServerAddr.sin_port = htons(newPort);
+	newServerAddr.sin_family = AF_INET;
+	newServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	newServerAddr.sin_port = htons(newPort);
 
-    if ((newSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        std::cerr << "Socket creation failed" << std::endl;
-        return;
-    }
-    if (bind(newSocket, (const struct sockaddr *)&newServerAddr, sizeof(newServerAddr)) < 0) {
-        std::cerr << "Binding failed" << std::endl;
-        return;
-    }
-    usedPorts.insert(newPort);
-    mtx.unlock();
+	if ((newSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		std::cerr << "Socket creation failed" << std::endl;
+		mtxPorts.lock();
+		usedPorts.erase(newPort);
+		mtxPorts.unlock();
+		mtxThreads.unlock();
+		return;
+	}
+	if (bind(newSocket, (const struct sockaddr *)&newServerAddr, sizeof(newServerAddr)) < 0) {
+		std::cerr << "Binding failed" << std::endl;
+		mtxPorts.lock();
+		usedPorts.erase(newPort);
+		mtxPorts.unlock();
+		mtxThreads.unlock();
+		return;
+	}
+	mtxPorts.lock();
+	usedPorts.insert(newPort);
+	mtxPorts.unlock();
+	mtxThreads.unlock();
 
-    n = recvfrom(newSocket, (char *)clientMsg, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &len);
+	int n = recvfrom(newSocket, (char *)clientMsg, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&clientAddr, &len);
+	clientMsg[n] = '\0';
 
-    char* endp;
-    double X = strtod(clientMsg, &endp);
-    std::unordered_set<double> uniqueDoubles = generateUniqueDoubles(X);
+	char* endp;
+	double X = strtod(clientMsg, &endp);
+	std::unordered_set<double> uniqueDoubles;
 
-    sendUniqueDoubles(sockfd, client_addr, len, uniqueDoubles);
+	generateUniqueDoubles(X, uniqueDoubles);
+	sendUniqueDoubles(newSocket, clientAddr, len, uniqueDoubles);
+	close(newSocket);
+	mtxPorts.lock();
+	usedPorts.erase(newPort);
+	mtxPorts.unlock();
 }
 
 int findAvailablePort(const std::unordered_set<int>& usedPorts) {
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Socket creation failed" << std::endl;
-        return -1;
-    }
+	mtxPorts.lock();
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in addr;
 
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    for (int port = PORT_RANGE_START; port <= PORT_RANGE_END; ++port) {
-        if (usedPorts.find(port) == usedPorts.end()) {
-            addr.sin_port = htons(port);
-            if (bind(sockfd, (const struct sockaddr *)&addr, sizeof(addr)) == 0) {
-                close(sockfd);
-                std::cout << port << std::endl;
-                return port;
-            }
-        }
-    }
+	if (sockfd < 0) {
+		std::cerr << "Socket creation failed" << std::endl;
+		mtxPorts.unlock();
+		return -1;
+	}
 
-    close(sockfd);
-    return -1;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	for (int port = PORT_RANGE_START; port <= PORT_RANGE_END; ++port) {
+		if (usedPorts.find(port) == usedPorts.end()) {
+			addr.sin_port = htons(port);
+			if (bind(sockfd, (const struct sockaddr *)&addr, sizeof(addr)) == 0) {
+				close(sockfd);
+				mtxPorts.unlock();
+				return port;
+			}
+		}
+	}
+
+	close(sockfd);
+	mtxPorts.unlock();
+	return -1;
 }
 
 int getPortFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return -1;
-    }
+	std::ifstream file(filename);
 
-    std::string line;
-    while (std::getline(file, line)) {
-        size_t pos = line.find("Port : ");
-        if (pos != std::string::npos) {
-            std::string portStr = line.substr(pos + 7);
-            return std::atoi(portStr.c_str());
-        }
-    }
+	if (!file.is_open()) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return -1;
+	}
 
-    std::cerr << "Port not found in file: " << filename << std::endl;
-    return -1;
+	std::string line;
+	while (std::getline(file, line)) {
+		size_t pos = line.find("Port : ");
+		if (pos != std::string::npos) {
+			std::string portStr = line.substr(pos + 7);
+			return std::atoi(portStr.c_str());
+		}
+	}
+
+	std::cerr << "Port not found in file: " << filename << std::endl;
+	return -1;
 }
